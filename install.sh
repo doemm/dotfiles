ce9315b96f4f0d18fe193cc670e80ba4af8f3597
domsupamoe <0jinwei0.wang@gmail.com>
2022-12-24 11:43:06 -0800

    feat: add net-tools to install packages list

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo -e "Update and upgrade outdated packages"
 sudo apt -y update && sudo apt -y upgrade && echo -e "\n"
 
 # install default packages
-packages="zsh git vim tmux neovim "
+packages="zsh git vim tmux neovim net-tools "
 packages+="fzf fd-find bat ripgrep exa ranger neofetch"
 
 echo -e "Install packages in a dry-run: $packages"
